3390d01447462e2d3c47fb552e89d7167c373606
Sanjay-Varma-OX <sanjay.varma@oxmaint.com>
2024-12-13 13:28:53 -0800

    Update server.js with improved error handling and logging

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // For any request that doesn't match one above, send back React's index.html file.
 app.get('/*', (req, res) => {
+  console.log('Serving index.html for path:', req.path);
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).send('Server Error');
 });
 
 app.listen(PORT, () => {
