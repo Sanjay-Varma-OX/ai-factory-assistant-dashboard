146a6172be1b127c4fa237c595fe3383ec807f2b
Sanjay-Varma-OX <sanjay.varma@oxmaint.com>
2024-12-13 11:57:48 -0800

    Update server.js with proper routing configuration

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const PORT = process.env.PORT || 8080;
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+// For any request that doesn't match one above, send back React's index.html file.
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log('Current directory:', __dirname);
+  console.log('Build path:', path.join(__dirname, 'build'));
 });
