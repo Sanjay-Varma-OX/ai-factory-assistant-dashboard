ee438dd5067f812a34f2934c88b50397c0f38a29
Sanjay-Varma-OX <sanjay.varma@oxmaint.com>
2024-12-13 00:41:27 -0800

    Add main App component with routing structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+import LandingPage from './pages/LandingPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App min-h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
