aa241bfc541f5532f4917347a5a1f1f5270c0966
Sanjay-Varma-OX <sanjay.varma@oxmaint.com>
2024-12-13 12:40:29 -0800

    Add Azure deployment workflow and update server configuration

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// For any request that doesn't match one above, send back React's index.html file.
+// For any request that doesn't match one above, send back React's index.html file
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(PORT, () => {
