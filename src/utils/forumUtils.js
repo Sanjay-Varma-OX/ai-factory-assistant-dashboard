1f4fb85471dac23c2bd8e774b14bdda979bb2fcc
Sanjay-Varma-OX <sanjay.varma@oxmaint.com>
2025-01-08 17:03:59 -0800

    Update forumUtils.js

--- src/utils/forumUtils.js
+++ src/utils/forumUtils.js
@@ -112,27 +112,3 @@ const checkThreadExists = async (threadId) => {
     return false;
   }
 };
-
-// Export it if needed by other components
-export const getQuickThreadCount = async () => {
-  try {
-    let count = 0;
-    let consecutiveFailures = 0;
-    let currentId = 1;
-
-    while (consecutiveFailures < 3) {
-      const exists = await checkThreadExists(currentId);
-      if (exists) {
-        count++;
-        consecutiveFailures = 0;
-      } else {
-        consecutiveFailures++;
-      }
-      currentId++;
-    }
-    return count;
-  } catch (error) {
-    console.error('Error getting thread count:', error);
-    return 0;
-  }
-};
